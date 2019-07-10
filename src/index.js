c1f86a231bf91f77af6f93975a85040b35dacd17
shaho <toofani@gmail.com>
2019-07-10 17:20:54 +0430

    Configure nodemon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log("Hello GraphQl");
+import { GraphQLServer } from "graphql-yoga";
+
+// Scalar types - String, Boolean, Int, Float, ID
+
+// Type defenintions (Schema)
+const typeDefs = `
+  type Query {
+    id: ID!
+    name: String!
+    age: Int!
+    employed: Boolean!
+    gpa: Float
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    id() {
+      return "abc123";
+    },
+    name() {
+      return "Andrew";
+    },
+    age() {
+      return 27;
+    },
+    employed() {
+      return true;
+    },
+    gpa() {
+      return null;
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("The server is up");
+});
