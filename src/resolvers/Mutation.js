a3e6f2a2f2b40dad4275ecf9b37d3ad528decebb
shaho <toofani@gmail.com>
2019-07-12 01:49:53 +0430

    Add updating data with mutation, last commit in this section - Sec. 3, Ep. 11

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -87,6 +87,17 @@ const Mutation = {
     return deletedPost[0];
   },
 
+  updatePost(parent, { id, data }, { db }, info) {
+    const post = db.posts.find((post) => post.id === id);
+    if (!post) throw new Error("Post not found");
+
+    if (typeof data.title === "string") post.title = data.title;
+    if (typeof data.body === "string") post.body = data.body;
+    if (typeof data.published === "boolean") post.published = data.published;
+
+    return post;
+  },
+
   createComment(parent, args, { db }, info) {
     const userExist = db.users.some((user) => user.id === args.data.author);
     const postExist = db.posts.some((post) => {
@@ -117,6 +128,15 @@ const Mutation = {
     const deletedComments = db.comments.splice(commentIndex, 1);
 
     return deletedComments[0];
+  },
+
+  updateComment(parent, { id, data }, { db }, info) {
+    const comment = db.comments.find((comment) => comment.id === id);
+    if (!comment) throw new Error("Comment not found");
+
+    if (typeof data.text === "string") comment.text = data.text;
+
+    return comment;
   },
 };
 
