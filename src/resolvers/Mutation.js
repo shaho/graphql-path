d49ec3bac48478d3a0460606122f185eb5b60bfc
shaho <toofani@gmail.com>
2019-07-12 01:06:09 +0430

    Update data with mutation part one - Sec. 3, Ep. 10

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -41,6 +41,23 @@ const Mutation = {
     return deletedUser[0];
   },
 
+  updateUser(parent, { id, data }, { db }, info) {
+    const user = db.users.find((user) => user.id === id);
+    if (!user) throw new Error("User not found");
+
+    if (typeof data.email === "string") {
+      const emailTaken = db.users.some((user) => user.email === data.email);
+      if (emailTaken) throw new Error("Email taken");
+      user.email = data.email;
+    }
+
+    if (typeof data.name === "string") user.name = data.name;
+
+    if (typeof data.age !== "undefined") user.age = data.age;
+
+    return user;
+  },
+
   createPost(parent, args, { db }, info) {
     const userExist = db.users.some((user) => user.id === args.data.author);
 
