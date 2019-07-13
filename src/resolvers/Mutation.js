2be3eedcfb6a68accf696d7b1b33b1017b8dba47
shaho <toofani@gmail.com>
2019-07-14 02:44:05 +0430

    Expand the Comments Subscription with Edits and Deletions - Sec. 4, Ep. 6

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -170,13 +170,16 @@ const Mutation = {
     // Implement subscription comment
     // This would be run if a specified - published - post has a new comment
     pubsub.publish(`comment ${args.data.post}`, {
-      comment: comment,
+      comment: {
+        mutation: "CREATED",
+        data: comment,
+      },
     });
 
     return comment;
   },
 
-  deleteComment(parent, args, { db }, info) {
+  deleteComment(parent, args, { db, pubsub }, info) {
     const commentIndex = db.comments.findIndex(
       (comment) => comment.id === args.id,
     );
@@ -184,16 +187,30 @@ const Mutation = {
     if (commentIndex === -1) {
       throw new Error("Comment not found");
     }
-    const deletedComments = db.comments.splice(commentIndex, 1);
+    const [deletedComment] = db.comments.splice(commentIndex, 1);
 
-    return deletedComments[0];
+    pubsub.publish(`comment ${deletedComment.post}`, {
+      comment: {
+        mutation: "DELETED",
+        data: deletedComment,
+      },
+    });
+
+    return deletedComment;
   },
 
-  updateComment(parent, { id, data }, { db }, info) {
+  updateComment(parent, { id, data }, { db, pubsub }, info) {
     const comment = db.comments.find((comment) => comment.id === id);
     if (!comment) throw new Error("Comment not found");
 
     if (typeof data.text === "string") comment.text = data.text;
+
+    pubsub.publish(`comment ${comment.post}`, {
+      comment: {
+        mutation: "UPDATED",
+        data: comment,
+      },
+    });
 
     return comment;
   },
